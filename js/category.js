49a80ff9b70c7d5d5bba59d8c947e801032cc7ed
Rifazul <rifazul50@gmail.com>
2022-09-03 03:29:20 +0600

    add card

--- js/category.js
+++ js/category.js
@@ -16,7 +16,7 @@ const displayShow = (data) => {
 
     data.forEach(data => {
 
-        console.log(data)
+        // console.log(data)
 
         const newDiv = document.createElement('div')
 
@@ -32,4 +32,55 @@ const displayShow = (data) => {
 }
 
 loadCatgory()
+
+const cotagoriesId = () => {
+
+    fetch('https://openapi.programming-hero.com/api/news/category/01')
+
+        .then(res => res.json())
+        .then(data => cotagoriesIdShow(data.data))
+
+}
+
+const cotagoriesIdShow = (data) => {
+
+    // console.log(data)
+
+    const showContainer = document.getElementById('show-container')
+
+    showContainer.innerHTML = ``;
+
+    data.forEach(data => {
+
+        console.log(data)
+
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+        <div class="card mb-3" style="width: 1120px;">
+              <div class="row g-4">
+              <div class="col-md-4">
+                  <img src="${data.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                  <div class="card-body">
+                      <h5 class="card-title"> ${data.title}</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                          additional content. This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+              </div>
+          </div>
+          </div>
+              
+              `;
+
+        showContainer.appendChild(div)
+    })
+
+}
+
+
+cotagoriesId()
+
 
